f108afe2502eb8946ddca33988485d3aacd2ad54
Mihai Dusmanu <mihai.dusmanu@yahoo.com>
2018-01-06 17:59:16 +0100

    Added USPS data-set testing file

---
+++ usps.py
@@ -0,0 +1,49 @@
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+
+import anchors_SSL
+import fuzzy_cmeans_anchors
+import kmeans_anchors
+import LAE
+import random_anchors
+import util
+
+def read_usps(file_path):
+    x = []
+    labels = []
+    f = open(file_path, 'r')
+    reader = csv.reader(f, delimiter = ' ')
+    for row in reader:
+        labels.append(int(float(row[0])))
+        x.append([float(v) for v in row[1:-1]])
+    f.close()
+    return np.array(x), np.array(labels)
+    
+K = 10
+m = 1000
+l = 100
+
+X, ground_truth = read_usps('data/zip.train')
+
+acc = 0
+
+for t in range(20):
+    #anchors, Z = random_anchors.find(X, m, 1)
+    anchors, Z = kmeans_anchors.find(X, m, 1)
+    #anchors, Z = fuzzy_cmeans_anchors.find(X, m, 1.1)
+    #anchors, Z = LAE.find(X, m, 2)
+    
+    labeled = util.random_choice(l, K, ground_truth)
+    Y = np.zeros((l, K))
+    for id, real_id in enumerate(labeled):
+        Y[id, ground_truth[real_id]] = 1
+    
+    pred = anchors_SSL.predict(0.040, Z, labeled, Y)
+    
+    current_acc = util.accuracy(ground_truth, pred)
+    acc += current_acc
+    
+    print("[Try #" + str(t) + "] " + str(current_acc))
+
+print("Average accuracy over 20 tries: " + str(acc / 20))
