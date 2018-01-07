6c3e1fd53ecee198ca398fb81bf40a370403f260
Mihai Dusmanu <mihai.dusmanu@yahoo.com>
2018-01-07 10:54:18 +0100

    Fixed a bug in random anchors; consider only s-NN for kernel approach

--- kmeans_anchors.py
+++ kmeans_anchors.py
@@ -6,13 +6,17 @@ def gaussian_kernel(X, Y, sigma):
     sqdist = scipy.spatial.distance.cdist(X, Y, metric = 'sqeuclidean')
     return np.exp(- 1 / (2 * sigma**2) * sqdist)
 
-def find(X, nb_anchors, sigma):
+def find(X, nb_anchors, sigma, s):
     kmeans = KMeans(n_clusters = nb_anchors).fit(X)
     anchors = kmeans.cluster_centers_
-    Z = gaussian_kernel(X, anchors, sigma)
+    Z_full = gaussian_kernel(X, anchors, sigma)
+    Z = np.zeros(Z_full.shape)
+    for i in range(Z.shape[0]):
+        i_nn = np.argsort(-Z_full[i, :])[0 : s]
+        Z[i, i_nn] = Z_full[i, i_nn]
     Z /= np.transpose(np.sum(Z, 1)[np.newaxis])
     return anchors,Z
 
 if __name__ == '__main__':
     X = np.array([[0, 0], [1, 0], [0, 1], [1, 1]])
-    print(find(X, 2))
+    print(find(X, 2, 1, 1))
--- random_anchors.py
+++ random_anchors.py
@@ -5,13 +5,14 @@ def gaussian_kernel(X,Y,sigma):
     sqdist = scipy.spatial.distance.cdist(X,Y,metric='sqeuclidean')
     return np.exp(-1/(2*sigma**2) * sqdist)
 
-def kernel_weights(X,anchors,ker):
-    Z = ker(X,anchors)
-    return Z/np.transpose(np.sum(Z, 1)[np.newaxis])
-
-def find(X,nb_anchors,sigma):
-    anchors = [X[i,:] for i in np.random.choice(range(nb_anchors),nb_anchors,replace=False)]
-    Z = kernel_weights(X,anchors,lambda X,Y: gaussian_kernel(X,Y,sigma))
+def find(X,nb_anchors,sigma,s):
+    anchors = [X[i,:] for i in np.random.choice(range(X.shape[0]),nb_anchors,replace=False)]
+    Z_full = gaussian_kernel(X,anchors,sigma)
+    Z = np.zeros(Z_full.shape)
+    for i in range(Z.shape[0]):
+        i_nn = np.argsort(-Z_full[i, :])[0:s]
+        Z[i,i_nn] = Z_full[i,i_nn]
+    Z /= np.transpose(np.sum(Z, 1)[np.newaxis])
     return np.array(anchors),np.array(Z)
 
 if __name__ == '__main__':
